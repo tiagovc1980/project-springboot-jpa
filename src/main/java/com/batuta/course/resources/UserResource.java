b30af3362544d502bf0335d60ebccfad22efd420
Tiago VC <vc.tiago@gmail.com>
2023-01-16 11:00:36 -0300

    User entity and resource

---
+++ src/main/java/com/batuta/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.batuta.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.batuta.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		
+		User u = new User(1L, "Maria", "maria@gmail.com", "(16)9999-9999", "1234");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+}
