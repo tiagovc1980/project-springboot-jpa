6202868a0e9d3ff1c58ad7953b35d81c95e25e60
Tiago VC <vc.tiago@gmail.com>
2023-01-17 17:45:36 -0300

    Payment OneToOne Association

--- src/main/java/com/batuta/course/config/TestConfig.java
+++ src/main/java/com/batuta/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.batuta.course.entities.Category;
 import com.batuta.course.entities.Order;
 import com.batuta.course.entities.OrderItem;
+import com.batuta.course.entities.Payment;
 import com.batuta.course.entities.Product;
 import com.batuta.course.entities.User;
 import com.batuta.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
