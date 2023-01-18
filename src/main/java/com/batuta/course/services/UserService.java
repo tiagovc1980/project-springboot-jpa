1f0470ba9e7c1a33756265d194a941cf99d863d0
Tiago VC <vc.tiago@gmail.com>
2023-01-18 10:40:41 -0300

    Exception handling - findById

--- src/main/java/com/batuta/course/services/UserService.java
+++ src/main/java/com/batuta/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.batuta.course.entities.User;
 import com.batuta.course.repositories.UserRepository;
+import com.batuta.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById (Long Id) {
 		Optional<User> obj = repository.findById(Id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
 	
 	public User insert (User obj) {
